d0b952e9777e1ad06a67ff79324261a1a8b8da6e
LXY113 <36401246+LXY113@users.noreply.github.com>
2025-08-13 21:01:54 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "小雨的Paper";
+export const SITE_DESCRIPTION = "欢迎！这里是小雨的Paper，KOSAME's Paper";
