58099e1500cb1c43dcaa98f66d97e490dc12f152
LXY113 <36401246+LXY113@users.noreply.github.com>
2025-08-14 11:06:26 +0800

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,6 +4,18 @@ import { defineCollection, z } from "astro:content";
 const blog = defineCollection({
   // Load Markdown and MDX files in the `src/content/blog/` directory.
   loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
+});
+const pages = defineCollection({
+  // Load Markdown and MDX files in the `src/content/pages/` directory.
   loader: glob({ base: "./src/content/pages", pattern: "**/*.{md,mdx}" }),
   // Type-check frontmatter using a schema
   schema: z.object({
@@ -16,4 +28,4 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+export const collections = { blog , pages };
